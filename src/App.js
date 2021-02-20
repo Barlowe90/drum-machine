42fdebbc336f88f07ee82d7215c4ee2ceb047fd9
Barlowe <67549375+Barlowe90@users.noreply.github.com>
2021-02-20 16:03:58 +0100

    Update App.js
    logic base

--- src/App.js
+++ src/App.js
@@ -1,8 +1,43 @@
+import "bootstrap/dist/css/bootstrap.min.css";
+
 function App() {
   return (
-    <div className="App">
-      <h2>A drum machine</h2>
-      <p>By Adri engineer</p>
+    <div className="container my-5" id="drum-machine">
+      <div id="display">
+        <div className="d-flex align-items-center justify-content-center">
+          <div className="p-2 bd-highlight drum-pad" id="senhora">
+            Q
+          </div>
+          <div className="p-2 bd-highlight drum-pad" id="tetas">
+            W
+          </div>
+          <div className="p-2 bd-highlight drum-pad" id="chetos">
+            E
+          </div>
+        </div>
+        <div className="d-flex align-items-center justify-content-center">
+          <div className="p-2 bd-highlight drum-pad" id="maquina">
+            A
+          </div>
+          <div className="p-2 bd-highlight drum-pad" id="lince">
+            S
+          </div>
+          <div className="p-2 bd-highlight drum-pad" id="ouch">
+            D
+          </div>
+        </div>
+        <div className="d-flex align-items-center justify-content-center">
+          <div className="p-2 bd-highlight drum-pad" id="fiera">
+            Z
+          </div>
+          <div className="p-2 bd-highlight drum-pad" id="yuhu">
+            X
+          </div>
+          <div className="p-2 bd-highlight drum-pad" id="rey">
+            C
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
