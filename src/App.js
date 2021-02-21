837d8c229cfca6dcd808259d6e2980fbb5cc0dfe
Barlowe <67549375+Barlowe90@users.noreply.github.com>
2021-02-21 21:01:04 +0100

    Update App.js
    just added button

--- src/App.js
+++ src/App.js
@@ -1,70 +1,52 @@
 import "bootstrap/dist/css/bootstrap.min.css";
 
 function App() {
+  let audio = new Audio("./audio/si-mac.m4a");
+
   return (
-    <div className="container my-5" id="drum-machine">
+    <div className="container" id="drum-machine">
+      <h1 className="text-center">A drum machine</h1>
       <div id="display">
         <div
           className="d-flex align-items-center justify-content-center btn-group"
           role="group"
         >
-          <button className="btn btn-dark">
-            <div className="p-2 bd-highlight drum-pad" id="senhora">
-              Q
-              <audio className="clip" id="Q">
-                <source src="" type=""></source>
-              </audio>
-            </div>
+          <button className="btn btn-dark drum-pad" id="Q">
+            Q<audio className="clip" id="Q"></audio>
           </button>
-          <button className="btn btn-dark">
-            <div className="p-2 bd-highlight drum-pad" id="no">
-              W
-            </div>
+          <button className="btn btn-dark drum-pad" id="no">
+            W<audio className="clip" id="W" src="./audio/si-mac.m4a"></audio>
           </button>
-          <button className="btn btn-dark">
-            <div className="p-2 bd-highlight drum-pad" id="chetos">
-              E
-            </div>
+          <button className="btn btn-dark drum-pad" id="chetos">
+            E<audio className="clip" id="E" src="./audio/si-mac.m4a"></audio>
           </button>
         </div>
         <div
           className="d-flex align-items-center justify-content-center btn-group"
           role="group"
         >
-          <button className="btn btn-dark">
-            <div className="p-2 bd-highlight drum-pad" id="maquina">
-              A
-            </div>
+          <button className="btn btn-dark drum-pad" id="maquina">
+            A<audio className="clip" id="A" src="./audio/si-mac.m4a"></audio>
           </button>
-          <button className="btn btn-dark">
-            <div className="p-2 bd-highlight drum-pad" id="lince">
-              S
-            </div>
+          <button className="btn btn-dark drum-pad" id="lince">
+            S<audio className="clip" id="S" src="./audio/si-mac.m4a"></audio>
           </button>
-          <button className="btn btn-dark">
-            <div className="p-2 bd-highlight drum-pad" id="teRies">
-              D
-            </div>
+          <button className="btn btn-dark drum-pad" id="teRies">
+            D<audio className="clip" id="D" src="./audio/si-mac.m4a"></audio>
           </button>
         </div>
         <div
           className="d-flex align-items-center justify-content-center btn-group"
           role="group"
         >
-          <button className="btn btn-dark">
-            <div className="p-2 bd-highlight drum-pad" id="fiera">
-              Z
-            </div>
+          <button className="btn btn-dark drum-pad" id="fiera">
+            Z<audio className="clip" id="Z" src="./audio/si-mac.m4a"></audio>
           </button>
-          <button className="btn btn-dark">
-            <div className="p-2 bd-highlight drum-pad" id="si">
-              X
-            </div>
+          <button className="btn btn-dark drum-pad" id="si">
+            X<audio className="clip" id="X" src="./audio/si-mac.m4a"></audio>
           </button>
-          <button className="btn btn-dark">
-            <div className="p-2 bd-highlight drum-pad" id="rey">
-              C
-            </div>
+          <button className="btn btn-dark drum-pad" id="rey">
+            C<audio className="clip" id="C" src="./audio/si-mac.m4a"></audio>
           </button>
         </div>
       </div>
