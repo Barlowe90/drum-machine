226abaf5bff12f67f5b6974e66edf104ce14e2fc
Barlowe <67549375+Barlowe90@users.noreply.github.com>
2021-02-21 19:31:50 +0100

    Update App.js
    adding button to each letter

--- src/App.js
+++ src/App.js
@@ -4,38 +4,68 @@ function App() {
   return (
     <div className="container my-5" id="drum-machine">
       <div id="display">
-        <div className="d-flex align-items-center justify-content-center">
-          <div className="p-2 bd-highlight drum-pad" id="senhora">
-            Q
-          </div>
-          <div className="p-2 bd-highlight drum-pad" id="tetas">
-            W
-          </div>
-          <div className="p-2 bd-highlight drum-pad" id="chetos">
-            E
-          </div>
+        <div
+          className="d-flex align-items-center justify-content-center btn-group"
+          role="group"
+        >
+          <button className="btn btn-dark">
+            <div className="p-2 bd-highlight drum-pad" id="senhora">
+              Q
+              <audio className="clip" id="Q">
+                <source src="" type=""></source>
+              </audio>
+            </div>
+          </button>
+          <button className="btn btn-dark">
+            <div className="p-2 bd-highlight drum-pad" id="no">
+              W
+            </div>
+          </button>
+          <button className="btn btn-dark">
+            <div className="p-2 bd-highlight drum-pad" id="chetos">
+              E
+            </div>
+          </button>
         </div>
-        <div className="d-flex align-items-center justify-content-center">
-          <div className="p-2 bd-highlight drum-pad" id="maquina">
-            A
-          </div>
-          <div className="p-2 bd-highlight drum-pad" id="lince">
-            S
-          </div>
-          <div className="p-2 bd-highlight drum-pad" id="ouch">
-            D
-          </div>
+        <div
+          className="d-flex align-items-center justify-content-center btn-group"
+          role="group"
+        >
+          <button className="btn btn-dark">
+            <div className="p-2 bd-highlight drum-pad" id="maquina">
+              A
+            </div>
+          </button>
+          <button className="btn btn-dark">
+            <div className="p-2 bd-highlight drum-pad" id="lince">
+              S
+            </div>
+          </button>
+          <button className="btn btn-dark">
+            <div className="p-2 bd-highlight drum-pad" id="teRies">
+              D
+            </div>
+          </button>
         </div>
-        <div className="d-flex align-items-center justify-content-center">
-          <div className="p-2 bd-highlight drum-pad" id="fiera">
-            Z
-          </div>
-          <div className="p-2 bd-highlight drum-pad" id="yuhu">
-            X
-          </div>
-          <div className="p-2 bd-highlight drum-pad" id="rey">
-            C
-          </div>
+        <div
+          className="d-flex align-items-center justify-content-center btn-group"
+          role="group"
+        >
+          <button className="btn btn-dark">
+            <div className="p-2 bd-highlight drum-pad" id="fiera">
+              Z
+            </div>
+          </button>
+          <button className="btn btn-dark">
+            <div className="p-2 bd-highlight drum-pad" id="si">
+              X
+            </div>
+          </button>
+          <button className="btn btn-dark">
+            <div className="p-2 bd-highlight drum-pad" id="rey">
+              C
+            </div>
+          </button>
         </div>
       </div>
     </div>
